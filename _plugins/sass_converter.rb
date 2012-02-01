b4cb50748c501d71abe7896dc6380b1f7caa0e9c
Martin Lowinski <martin@goldtopf.org>
2012-02-01 18:12:35 +0100

    Add Sass-Converter plugin.

---
+++ _plugins/sass_converter.rb
@@ -0,0 +1,31 @@
+module Jekyll
+  # Sass plugin to convert .scss to .css
+  # 
+  # Note: This is configured to use the new css like syntax available in sass.
+  require 'sass'
+  class SassConverter < Converter
+    safe true
+    priority :low
+
+    def matches(ext)
+      ext =~ /sass/i
+    end
+
+    def output_ext(ext)
+      ".css"
+    end
+
+    def convert(content)
+      begin
+        puts "Performing Sass Conversion."
+        # Without it the output is the default nested style, however you can
+        # choose either :expanded, :compact or :compressed as per the Output
+        # Style options from the sass reference page.
+        engine = Sass::Engine.new(content, :syntax => :sass, :load_paths => ["./css/"], :style => :compressed)
+        engine.render
+      rescue StandardError => e
+        puts "!!! SASS Error: " + e.message
+      end
+    end
+  end
+end
