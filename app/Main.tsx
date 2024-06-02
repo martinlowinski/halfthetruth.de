8819b107767b14f77f995b8b27e89a8690b41154
Martin Lowinski <mail@martinlowinski.de>
2024-06-02 15:11:50 +0200

    Fix css class attributes

--- app/Main.tsx
+++ app/Main.tsx
@@ -9,21 +9,21 @@ const MAX_DISPLAY = 5
 export default function Home({ posts }) {
   return (
     <>
-      <div class="space-y-2 pb-8 pt-6 md:space-y-5">
-        <h1 class="text-4xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:leading-10 md:leading-14">
-          Hey, I'm{' '}
+      <div className="space-y-2 pb-8 pt-6 md:space-y-5">
+        <h1 className="text-4xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:leading-10 md:leading-14">
+          Hey, I'm
           <a
-            class="hover-underline-animation text-accent"
+            className="hover-underline-animation text-accent"
             href="https://martinlowinski.de"
             target="_blank"
           >
             Martin
-          </a>{' '}
-          <span class="wave" role="img" aria-label="Hello">
+          </a>
+          <span className="wave" role="img" aria-label="Hello">
             👋🏻
           </span>
         </h1>
-        <p class="font-mono text-lg tracking-tighter text-gray-600 dark:text-gray-400">
+        <p className="font-mono text-lg tracking-tighter text-gray-600 dark:text-gray-400">
           I build digital products & businesses. For a greener planet.
         </p>
       </div>
