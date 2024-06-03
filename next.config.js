a9e85416c9289ebf7dc39d42acdc28f27302464c
Martin Lowinski <mail@martinlowinski.de>
2024-06-03 22:33:43 +0200

    Move CSP rule for plausible to script-src

--- next.config.js
+++ next.config.js
@@ -6,8 +6,8 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
-  default-src 'self' *.cloud.lowin.ski;
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is;
+  default-src 'self';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is plausible.cloud.lowin.ski;
   style-src 'self' 'unsafe-inline';
   img-src * blob: data:;
   media-src *.s3.amazonaws.com;
