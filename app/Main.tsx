3302a55dd060dc599d361cbfd010a9b7358309a2
Martin Lowinski <mail@martinlowinski.de>
2024-09-21 21:34:20 +0200

    Remove post tags from main page

--- app/Main.tsx
+++ app/Main.tsx
@@ -58,11 +58,6 @@ export default function Home({ posts }) {
                               {title}
                             </Link>
                           </h2>
-                          <div className="flex flex-wrap">
-                            {tags.map((tag) => (
-                              <Tag key={tag} text={tag} />
-                            ))}
-                          </div>
                         </div>
                         <div className="prose max-w-none text-gray-500 dark:text-gray-400">
                           {summary}
